b8b10fb3fb5587bd87ce967d9ccbca37dd01bc30
brianmondino <brianmondino@gmail.com>
2022-06-05 00:02:02 -0300

    commit
    modif de base.html y de create_prodocts.html

--- app_proyect/forms.py
+++ app_proyect/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from app_proyect.models import Products
 
- class Product_form(form.ModelForm):
+class Product_form(forms.ModelForm):
 	class Meta:
 		model = Products
 		fields = '__all__'
--- app_proyect/views.py
+++ app_proyect/views.py
@@ -1,9 +1,8 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 from app_proyect.models import Products
+from app_proyect.forms import Product_form
 
 def products(request):
         productos = Products.objects.all()
@@ -11,4 +10,25 @@ def products(request):
         return render(request, 'products.html', context=context)
 
 def create_product(request):
-	return render(request, 'create_product.html')
+	if request.method == 'GET':
+		form = Product_form()
+		context = {'form':form}
+		return render(request, 'create_product.html', context = context)
+	else:
+		form = Product_form(request.POST)
+		if form.is_valid():
+			new_product = Products.objects.create(
+				name = form.cleaned_data['name'],
+				proce = form.cleaned_data['price'],
+				description = form.cleaned_data['description'],
+				SKU = form.cleaned_data['SKU'],
+				active = form.cleaned_data['active'],
+				)
+			context = {'new_product':new_product}
+		return render(request, 'create_product.html', context = context)
+
+def search_product_view(request):
+	print(request.GET)
+	product = Products.objects.filter(name__icontains = request.GET['search'])
+	context = {'product':product}
+	return render (request, 'search_product.html', context = context)
